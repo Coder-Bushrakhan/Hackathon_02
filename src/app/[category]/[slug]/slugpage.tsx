50e1b5bca7c57d870cf632d5d1dc2904d5d68094
Coder-Bushrakhan <bk.bushra555@gmail.com>
2023-10-19 23:29:43 -0700

    update

--- src/app/[category]/[slug]/slugpage.tsx
+++ src/app/[category]/[slug]/slugpage.tsx
@@ -69,20 +69,20 @@ const Slugpage = ({ params }: { params: { slug: string } }) => {
                     </p>
                     <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                         <div className="flex">
-                            {/* <span className="mr-3"> {slug[0].color.map((item,i)=>
+                            <span className="mr-3"> {slug[0].color.map((item,i)=>
                             <button key={i} className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none" 
                             
                         style={{backgroundColor:item}}    
                             />
                         
                             
-                            )} </span> */}
+                            )} </span>
                            
                         </div>
                         <div className="flex ml-6 items-center">
                          
                             <div className="relative">
-                                {/* <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                                <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
                                 {slug[0].size.map((item,i)=>
                                 
                                 <option key={i}>{item}</option>
@@ -92,7 +92,7 @@ const Slugpage = ({ params }: { params: { slug: string } }) => {
 
 
 
-                                </select> */}
+                                </select>
 
                                 <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
                                     <svg
