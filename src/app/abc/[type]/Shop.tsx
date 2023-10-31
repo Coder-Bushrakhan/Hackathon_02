bf71677caefa3794fa5dde970988c827a8d42ce6
Coder-Bushrakhan <bk.bushra555@gmail.com>
2023-11-01 00:03:36 +0500

    update

--- src/app/abc/[type]/Shop.tsx
+++ src/app/abc/[type]/Shop.tsx
@@ -10,12 +10,11 @@ const Shop = ({ params }: { params: { type: string } }) => {
 
   return (
     <div>
-      <div className="flex flex-wrap flex justify-center mx-16">
+      {/* <div className="flex flex-wrap flex justify-center mx-16"> */}
         {/* <h1 className="mt-10 mb-10 text-4xl font-sans font-bold underline">{params.category }</h1> */}
 
-      </div>
-
-      <div className="container grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-3 mx-auto">
+    
+        <div className="container grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-3 mx-auto">
         {productlist.map((item: any, i) => (
           <ProductCard key={i} src={item.image} alt={item.title} title={item.title} description={item.description} price={item.price} category={item.category} slug={item.slug} />
 
